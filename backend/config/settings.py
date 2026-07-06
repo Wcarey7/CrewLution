1752062d745bc7dad650b9b541cfa57a1b2977f5
Wcarey7 <wadecarey7@gmail.com>
2026-07-06 14:23:48 -0700

    Common folder fixes. CSS for marketing only

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'common',
-    'marketing',
+    'common.app.CommonConfig',
+    'marketing.app.MarketingConfig',
+    'accounts.app.AccountsConfig',
 ]
 
 MIDDLEWARE = [
